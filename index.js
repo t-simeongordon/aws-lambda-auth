e0a10304928938d0012a5f398f3562783a2c6037
t s g <tsg@tsg.home>
2022-01-05 16:51:37 +0000

    updated params

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 
+// A simple token-based authorizer example to demonstrate how to use an authorization token 
+// to allow or deny a request. In this example, the caller named 'user' is allowed to invoke 
+// a request if the client-supplied token value is 'allow'. The caller is not allowed to invoke 
+// the request if the token value is 'deny'. If the token value is 'unauthorized' or an empty
+// string, the authorizer function returns an HTTP 401 status code. For any other token value, 
+// the authorizer returns an HTTP 500 status code. 
+// Note that token values are case-sensitive.
+
 const AWS = require('aws-sdk');
 
 
@@ -6,34 +14,41 @@ exports.handler =  async function(event, context, callback) {
     const Name = '/Project/Service/AuthToken';
     const options = { endpoint: 'https://ssm.eu-west-2.amazonaws.com'};
     let response = {};
-
+    
     try{
         console.log(`events: ${JSON.stringify(event)}`);
-        const result = new AWS.SSM(options).getParameter({Name}).promise();
-        console.log(`new AWS.SSM(options).getParameter({Name}).promise(): ${JSON.stringify(result)}`);
+        const paramToken = await new AWS.SSM(options).getParameter({Name}).promise();
+        console.log(`new AWS.SSM(options).getParameter({Name}).promise(): ${JSON.stringify(paramToken)}`);
+        
         var token = event.authorizationToken;
-        console.log(`new token: ${JSON.stringify(token)}`);
+        console.log(`type of new token: ${typeof token}`);
+        console.log(`type of paramToken: ${typeof paramToken}`);
 
-        if(token === null){
-            console.log(`token: null`);
-            response = "Error: Token not defined"
-        }
-        if(token === '1234'){
+        if(token === paramToken.Parameter.Value){
             console.log(`token: allow`);
-            response = generatePolicy('user', 'Allow', event.methodArn);
-            console.log(`token: allow: response: ${JSON.stringify(response)}`);
+            callback(null, generatePolicy('user', 'Allow', event.methodArn));
         }else{
             console.log(`token: deny`);
-            response = generatePolicy('user', 'Deny', event.methodArn);
-            console.log(`token: deny: response: ${JSON.stringify(response)}`);
+            callback(null, generatePolicy('user', 'Deny', event.methodArn));
         }
     }catch(error){
         console.error(`Main function caught error: ${error}`);
     }
     
-        console.log(`response: ${JSON.stringify(response)}`);
-
-    return response;
+    
+    // switch (token) {
+    //     case 'allow':
+    //         callback(null, generatePolicy('user', 'Allow', event.methodArn));
+    //         break;
+    //     case 'deny':
+    //         callback(null, generatePolicy('user', 'Deny', event.methodArn));
+    //         break;
+    //     case 'unauthorized':
+    //         callback("Unauthorized");   // Return a 401 Unauthorized response
+    //         break;
+    //     default:
+    //         callback("Error: Invalid token"); // Return a 500 Invalid token response
+    // }
 };
 
 // Help function to generate an IAM policy
